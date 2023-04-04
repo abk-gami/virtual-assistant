23d9563cebac19b392b9079d4a208b78265e1ecf
Abk <127848306+abk-gami@users.noreply.github.com>
2023-04-04 18:24:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,74 @@
 const speakBtn = document.querySelector('button'),
-btn = document.querySelector('button h3'),
+// btn = document.querySelector('button h3'),
 talk = document.querySelector('h2');
 
+function speak(sentence) {
+    const text_speak = new SpeechSynthesisUtterance(sentence);
+
+    text_speak.rate = 1;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+
+}
+function greetMe() {
+    let day = new Date();
+    let hr = day.getHours();
+
+    if(hr >= 0 && hr < 12){
+       speak("Good Morning babe");
+    }
+    else if(hr >= 12 && hr <= 16){
+        speak("Good Afternoon babe");
+    }
+    else {
+        speak("Good Evening Abubakar");
+    }
+}
+
+window.addEventListener('load', ()=>{
+    speak("Activating Sepab")
+    speak("Going Online")
+    greetMe();
+})
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
 const recognition = new SpeechRecognition();
 
-recognition.onstart = function () {
-   console.log('speech recognition started');
-   btn.textContent = "Listening...";
-
-};
-
-
-recognition.onresult = function (event) {
-   console.log(event);
-   const spokenwords = event.results[0][0].transcript;
-
-   console.log('Spoken Words are',spokenwords);
-   talk.textContent = spokenwords;
-   btn.textContent = "Speak";
-   computerSpeech(spokenwords);
+recognition.onresult = (event) => {
+    const current = event.resultIndex;
+    const transcript = event.results[current][0].transcript;
+    talk.textContent = transcript;
+    speakThis(transcript.toLowerCase());
 }
 
-function computerSpeech(words) {
-   const speech = new SpeechSynthesisUtterance();
-   speech.lang = "en-US"
-   speech.pitch = 0.9;
-   speech.volume = 1;
-   speech.rate = 1;
+speakBtn.addEventListener('click', ()=>{
+    recognition.start();
+})
 
-   determineWords(speech, words);
-   // speech.text = words;
+function speakThis(message) {
+    const speech = new SpeechSynthesisUtterance();
 
-   window.speechSynthesis.speak(speech);
+    speech.text = "I did not understand what you said please try again";
+
+    if(message.includes('hey') || message.includes('hello')) {
+        const finalText = "Hello boss";
+        speech.text = finalText;
+    }
+    else if(message.includes('how are you')) {
+        const finalText = "I am fine , thank you babe";
+        speech.text = finalText;
+    }
+    else if(message.includes('Who are you ')) {
+        const finalText = "I am f";
+        speech.text = finalText;
+    }
+
+
+
+    speech.volume = 1;
+    speech.pitch = 1;
+    speech.rate = 1;
+
+    window.speechSynthesis.speak(speech);
 }
-
-function determineWords(speech, words) {
-   if(words.includes("Hello")) {
-      speech.text = "Heyy";
-      btn.textContent = "Speak";
-   }
-
-   if(words.includes("What is your name")) {
-      speech.text = "My name is Giwa Favour";
-      btn.textContent = "Speak";
-   }
-   if(words.includes("How are you doing")) {
-      speech.text = "I am fine , thank you";
-      btn.textContent = "Speak";
-   }
-   if(words.includes("Who are you")) {
-      speech.text = "I am habeebee , ABK new girlfriend!";
-      btn.textContent = "Speak";
-   }
-}
-
-// function greetMe(){
-//    let date = new Date();
-//    let hr = date.getHours();
-
-//    if(hr >= 0 && hr < 12){
-//       computerSpeech("Good Morning Babe");
-//    }else if(hr >= 12 && hr <= 16){
-//       computerSpeech("Good Afternoon Babe");
-//    }else if(hr > 16 && hr < 12){
-//       computerSpeech("Good Evening Babe");
-//    }
-// }
-
-speakBtn.addEventListener('click', () => {
-   recognition.start();
-   talk.textContent = '....'
-}) 
-
-// window.addEventListener('load', ()=> {
-//    computerSpeech("Activating Sepab");
-//    greetMe();
-// })
-// window.addEventListener('load', greetMe);
